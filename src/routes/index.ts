6e2ff82fdd810c141bfba404baf0a949fa5a15fc
Adham Magdy <adhammagdy.dev@gmail.com>
2022-09-19 10:59:28 +0200

    setup and configured jasmine for unit testing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,8 +4,8 @@ import image_router from './api/image';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Welcome to the Image Processing API')
-})
+  res.send('Welcome to the Image Processing API');
+});
 
 router.use('/image', image_router);
 export default router;
