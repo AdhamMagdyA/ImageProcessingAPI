ce265690cc93401943feef65bb1a8c95b33f652e
Adham Magdy <adhammagdy.dev@gmail.com>
2022-09-19 10:12:01 +0200

    prepared endpoint for image displaying

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Image Processing API');
+});
+
+app.get('/api/image', (req, res) => {
+  const imageName = req.query.filename;
+  const imagePath = path.resolve(`./images/${imageName}.jpg`);
+  res.sendFile(imagePath);
 });
 
 app.listen(port, () => {
