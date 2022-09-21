21ced9d6486d6d7074c1b73ab67a04a4b8862227
Adham Magdy <adhammagdy.dev@gmail.com>
2022-09-21 13:34:31 +0200

    make thumb folder if does not exist

--- src/utils/imageProcessing.ts
+++ src/utils/imageProcessing.ts
@@ -1,10 +1,15 @@
 import sharp, { OutputInfo } from 'sharp';
+import fs from 'fs';
 
 const resizeImage = async (
   filename: string,
   width: number,
   height: number
 ): Promise<boolean> => {
+  // create thumb folder if it doesn't exist
+  if (!fs.existsSync('./assets/thumb')) {
+    fs.mkdirSync('./assets/thumb');
+  }
   // the resizing logic is taken from sharp documentation: https://sharp.pixelplumbing.com/api-constructor
   await sharp(`./assets/full/${filename}.jpg`)
     .resize(width, height)
